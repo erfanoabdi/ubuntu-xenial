f482c9790ad61c0888d0c54957fd12e5a2508dd9
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-09-08 16:13:45 -0700

    linux/kernel.h: move DIV_ROUND_DOWN_ULL() macro
    BugLink: https://bugs.launchpad.net/bugs/1852637
    
    This macro is useful to avoid link error on 32-bit systems.
    
    We have the same definition in two drivers, so move it to
    include/linux/kernel.h
    
    While we are here, refactor DIV_ROUND_UP_ULL() by using
    DIV_ROUND_DOWN_ULL().
    
    Link: http://lkml.kernel.org/r/1500945156-12907-1-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Mark Brown <broonie@kernel.org>
    Cc: Cyrille Pitchen <cyrille.pitchen@wedev4u.fr>
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Cc: Liam Girdwood <lgirdwood@gmail.com>
    Cc: Boris Brezillon <boris.brezillon@free-electrons.com>
    Cc: Marek Vasut <marek.vasut@gmail.com>
    Cc: Brian Norris <computersforpeace@gmail.com>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (backported from commit 604df322363e5770735df85368f83cac4a955a24)
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Khaled Elmously <khalid.elmously@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -65,8 +65,11 @@
 
 #define FIELD_SIZEOF(t, f) (sizeof(((t*)0)->f))
 #define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
-#define DIV_ROUND_UP_ULL(ll,d) \
-	({ unsigned long long _tmp = (ll)+(d)-1; do_div(_tmp, d); _tmp; })
+
+#define DIV_ROUND_DOWN_ULL(ll, d) \
+	({ unsigned long long _tmp = (ll); do_div(_tmp, d); _tmp; })
+
+#define DIV_ROUND_UP_ULL(ll, d)		DIV_ROUND_DOWN_ULL((ll) + (d) - 1, (d))
 
 #if BITS_PER_LONG == 32
 # define DIV_ROUND_UP_SECTOR_T(ll,d) DIV_ROUND_UP_ULL(ll, d)
