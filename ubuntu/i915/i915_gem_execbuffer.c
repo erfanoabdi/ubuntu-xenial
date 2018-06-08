8827c8fec779427ccdb41e7bb1020bf6cba463ba
Jon Bloomfield <jon.bloomfield@intel.com>
2018-06-08 10:05:26 -0700

    UBUNTU: SAUCE: i915_bpo: drm/i915: Remove Master tables from cmdparser
    The previous patch has killed support for secure batches
    on gen6+, and hence the cmdparsers master tables are
    now dead code. Remove them.
    
    Signed-off-by: Jon Bloomfield <jon.bloomfield@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    
    CVE-2019-0155
    
    [tjaalton: backport to i915_bpo
     - Modify *_reg_tables and find_reg(), use the 4.9-stable backport
       as inspiration.
     - The previous two cherry-picks were needed for this.]
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- ubuntu/i915/i915_gem_execbuffer.c
+++ ubuntu/i915/i915_gem_execbuffer.c
@@ -1179,8 +1179,7 @@ i915_gem_execbuffer_parse(struct intel_engine_cs *engine,
 			  struct eb_vmas *eb,
 			  struct drm_i915_gem_object *batch_obj,
 			  u32 batch_start_offset,
-			  u32 batch_len,
-			  bool is_master)
+			  u32 batch_len)
 {
 	struct drm_i915_gem_object *shadow_batch_obj;
 	struct i915_vma *vma;
@@ -1195,8 +1194,7 @@ i915_gem_execbuffer_parse(struct intel_engine_cs *engine,
 			      batch_obj,
 			      shadow_batch_obj,
 			      batch_start_offset,
-			      batch_len,
-			      is_master);
+			      batch_len);
 	if (ret)
 		goto err;
 
@@ -1564,8 +1562,7 @@ i915_gem_do_execbuffer(struct drm_device *dev, void *data,
 							     eb,
 							     batch_obj,
 							     args->batch_start_offset,
-							     args->batch_len,
-							     file->is_master);
+							     args->batch_len);
 		if (IS_ERR(parsed_batch_obj)) {
 			ret = PTR_ERR(parsed_batch_obj);
 			goto err;
