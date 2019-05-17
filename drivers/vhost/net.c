1801314e9de7435c941c0416227a0bf2f5121947
Jason Wang <jasowang@redhat.com>
2019-05-17 00:29:49 -0400

    vhost: introduce vhost_exceeds_weight()
    We used to have vhost_exceeds_weight() for vhost-net to:
    
    - prevent vhost kthread from hogging the cpu
    - balance the time spent between TX and RX
    
    This function could be useful for vsock and scsi as well. So move it
    to vhost.c. Device must specify a weight which counts the number of
    requests, or it can also specific a byte_weight which counts the
    number of bytes that has been processed.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    CVE-2019-3900
    
    (backported from commit e82b9b0727ff6d665fff2d326162b460dded554d)
    [tyhicks: Backport to Xenial:
     - Adjust handle_tx() instead of handle_tx_{copy,zerocopy}() due to
       missing commit 0d20bdf34dc7 ("vhost_net: split out datacopy logic")
     - Considerable context adjustments throughout the patch due to a lack
       of missing the iov_limit member of the vhost_dev struct which was
       added later in commit b46a0bf78ad7 ("vhost: fix OOB in get_rx_bufs()")
     - Context adjustment in call to vhost_log_write() in hunk #3 of net.c due to
       missing and unneeded commit cc5e71075947 ("vhost: log dirty page correctly")
     - Context adjustment in hunk #3 of net.c due to using break instead of goto
       out
     - Context adjustment in hunk #4 of net.c due to missing and unneeded commit
       c67df11f6e48 ("vhost_net: try batch dequing from skb array")
     - Don't patch vsock.c since Xenial doesn't have vhost vsock support
     - Adjust context in vhost_dev_init() to account for different local variables
     - Adjust context in struct vhost_dev to account for different struct members]
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -302,12 +302,6 @@ static bool vhost_exceeds_maxpend(struct vhost_net *net)
 	       min_t(unsigned int, VHOST_MAX_PEND, vq->num >> 2);
 }
 
-static bool vhost_exceeds_weight(int pkts, int total_len)
-{
-	return total_len >= VHOST_NET_WEIGHT ||
-	       pkts >= VHOST_NET_PKT_WEIGHT;
-}
-
 /* Expects to be always run from workqueue - which acts as
  * read-size critical section for our kind of RCU. */
 static void handle_tx(struct vhost_net *net)
@@ -431,10 +425,9 @@ static void handle_tx(struct vhost_net *net)
 		else
 			vhost_zerocopy_signal_used(net, vq);
 		vhost_net_tx_packet(net);
-		if (unlikely(vhost_exceeds_weight(++sent_pkts, total_len))) {
-			vhost_poll_queue(&vq->poll);
+		if (unlikely(vhost_exceeds_weight(vq, ++sent_pkts,
+						  total_len)))
 			break;
-		}
 	}
 out:
 	mutex_unlock(&vq->mutex);
@@ -655,10 +648,8 @@ static void handle_rx(struct vhost_net *net)
 		if (unlikely(vq_log))
 			vhost_log_write(vq, vq_log, log, vhost_len);
 		total_len += vhost_len;
-		if (unlikely(vhost_exceeds_weight(++recv_pkts, total_len))) {
-			vhost_poll_queue(&vq->poll);
+		if (unlikely(vhost_exceeds_weight(vq, ++recv_pkts, total_len)))
 			break;
-		}
 	}
 out:
 	mutex_unlock(&vq->mutex);
@@ -728,7 +719,8 @@ static int vhost_net_open(struct inode *inode, struct file *f)
 		n->vqs[i].vhost_hlen = 0;
 		n->vqs[i].sock_hlen = 0;
 	}
-	vhost_dev_init(dev, vqs, VHOST_NET_VQ_MAX);
+	vhost_dev_init(dev, vqs, VHOST_NET_VQ_MAX,
+		       VHOST_NET_WEIGHT, VHOST_NET_PKT_WEIGHT);
 
 	vhost_poll_init(n->poll + VHOST_NET_VQ_TX, handle_tx_net, POLLOUT, dev);
 	vhost_poll_init(n->poll + VHOST_NET_VQ_RX, handle_rx_net, POLLIN, dev);
